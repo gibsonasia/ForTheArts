5626be79478c9f55a6d8cb7dc721c04d1cb0a2bd
gibsonasia <gibsonjasia@gmail.com>
2018-02-06 11:34:30 -0500

    Updated Create artist account class and xml & updated artist landing page class and xml

--- app/src/main/java/com/studios/truhbel/forthearts/Artist/ArtistLandingPageActivity.java
+++ app/src/main/java/com/studios/truhbel/forthearts/Artist/ArtistLandingPageActivity.java
@@ -1,7 +1,11 @@
 package com.studios.truhbel.forthearts.Artist;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.studios.truhbel.forthearts.R;
 
@@ -11,10 +15,21 @@ import com.studios.truhbel.forthearts.R;
 
 public class ArtistLandingPageActivity extends AppCompatActivity {
 
+    ImageView profileImg;
+    TextView studioName,cityState, currentTV,recentTv;
+    CardView cardView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_page);
+
+        profileImg = findViewById(R.id.artist_iv);
+        studioName = findViewById(R.id.studio_tv);
+        cityState = findViewById(R.id.city_state_tv);
+        currentTV = findViewById(R.id.current_tv);
+        recentTv = findViewById(R.id.recent_tv);
+
     }
 }
--- app/src/main/java/com/studios/truhbel/forthearts/Artist/CreateArtistAccActivity.java
+++ app/src/main/java/com/studios/truhbel/forthearts/Artist/CreateArtistAccActivity.java
@@ -1,7 +1,13 @@
 package com.studios.truhbel.forthearts.Artist;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.studios.truhbel.forthearts.R;
 
@@ -11,9 +17,29 @@ import com.studios.truhbel.forthearts.R;
 
 public class CreateArtistAccActivity extends AppCompatActivity{
 
+    Spinner spinner;
+    EditText name, studioNsme, city, state;
+    Button confirmAccount;
+    TextView label;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_artist_acc);
+
+        spinner = findViewById(R.id.subject_spinner);
+        name = findViewById(R.id.et_name);
+        studioNsme = findViewById(R.id.et_studio_name);
+        city = findViewById(R.id.et_city);
+        confirmAccount = findViewById(R.id.confirm_btn);
+        label = findViewById(R.id.spinner_tv);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.subject_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
